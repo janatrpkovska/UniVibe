d776c7fadeb61ef54c556ab56930590e9a30fe32
janatr <janatrpkovska0@gmail.com>
2026-01-26 12:27:50 +0100

    feat: load categories dynamically from database

--- backend/src/main/java/com/univibe/backend/service/CategoryService.java
+++ backend/src/main/java/com/univibe/backend/service/CategoryService.java
@@ -5,10 +5,16 @@ import com.univibe.backend.model.Category;
 import java.util.List;
 
 public interface CategoryService {
+
     Category getCategoryById(Long id);
+
     List<Category> getAllCategories();
+
     Category getCategoryByName(String name);
+
     Category addCategory(String name, String icon_url);
+
     Category updateCategory(Long id, String name, String icon_url);
+
     Category deleteCategory(Long id);
 }
